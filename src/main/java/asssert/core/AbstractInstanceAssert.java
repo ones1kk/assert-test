369b46430cc3b4de0d22ec8b96908a93b7c9530b
ones1kk <kwsic@naver.com>
2022-03-25 00:22:40 +0900

    ✨ Add : Default prototype methods

--- src/main/java/asssert/core/AbstractInstanceAssert.java
+++ src/main/java/asssert/core/AbstractInstanceAssert.java
@@ -1,7 +1,35 @@
 package asssert.core;
 
-public abstract class AbstractInstanceAssert<SELF> implements InstanceAssert {
+public abstract class AbstractInstanceAssert<SELF, ACTUAL> {
 
-    protected boolean isInstanceof;
+    protected  SELF self;
+
+    protected  ACTUAL actual;
+
+    public AbstractInstanceAssert<SELF, ACTUAL> isNull() {
+        System.out.println("AbstractInstanceAssert.isNull = " + actual);
+        assert actual == null;
+        return this;
+    }
+
+    public AbstractInstanceAssert<SELF, ACTUAL> isEmpty() {
+        System.out.println("AbstractInstanceAssert.isEmpty = " + actual);
+        assert actual.toString().isEmpty();
+        return this;
+    }
+
+    public AbstractInstanceAssert<SELF, ACTUAL> isNotNull() {
+        System.out.println("AbstractInstanceAssert.isNull = " + actual);
+        assert actual != null;
+        return this;
+    }
+
+    public AbstractInstanceAssert<SELF, ACTUAL> isNotEmpty() {
+        System.out.println("AbstractInstanceAssert.isNull = " + actual);
+        assert !actual.toString().isEmpty();
+        return this;
+    }
+
+
 
 }

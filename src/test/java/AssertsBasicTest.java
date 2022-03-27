a1a18f8bed984bce76e03cbc80acf36e5530c489
ones1kk <kwsic@naver.com>
2022-03-28 00:21:57 +0900

    🚚 Rename : AssertsBasicTest

---
+++ src/test/java/AssertsBasicTest.java
@@ -0,0 +1,38 @@
+import asssert.Asserts;
+import asssert.core.AbstractInstanceAssert;
+import asssert.core.AbstractStringAssert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AssertsBasicTest {
+
+    @Test
+    @DisplayName("Assertions")
+    void test1() {
+        String str = "hello";
+        Assertions.assertThat(str).isNotNull().isNotEmpty().isNotBlank().isEqualTo("hello");
+    }
+
+    @Test
+    @DisplayName("Basic Structure")
+    void test2() {
+        String actual = "hello";
+
+        Asserts.assertThat(actual).isNotNull().isNotEmpty();
+//        Asserts.assertThat(actual).isNull().isEmpty();
+
+    }
+
+    @Test
+    @DisplayName("Is this assertion instance of AbstractInstanceAssert?")
+    void test3() {
+        String actual = "hello";
+        AbstractStringAssert<?, String> stringAbstractStringAssert = Asserts.assertThat(actual);
+
+        Assertions.assertThat(stringAbstractStringAssert).isInstanceOf(AbstractInstanceAssert.class);
+    }
+
+}
+
+

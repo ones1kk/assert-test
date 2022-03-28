b98cfc2b3733e674b472dc32e0aee06c3925e018
ones1kk <kwsic@naver.com>
2022-03-28 23:43:15 +0900

    ✅ Update : Basic structure test(failed)

--- src/test/java/AssertsBasicTest.java
+++ src/test/java/AssertsBasicTest.java
@@ -11,7 +11,8 @@ public class AssertsBasicTest {
     @DisplayName("Assertions")
     void test1() {
         String str = "hello";
-        Assertions.assertThat(str).as("actual was {}", str).isNotNull().isNotEmpty().isNotBlank().isEqualTo("hello");
+        Assertions.assertThat(str).as("actual was {}", str).isNotNull().isNotEmpty().isNotBlank()
+            .isEqualTo("hello");
     }
 
     @Test
@@ -19,8 +20,8 @@ public class AssertsBasicTest {
     void test2() {
         String actual = "hello";
 
-        Asserts.assertThat(actual).isNotNull().isNotEmpty();
-//        Asserts.assertThat(actual).isNull().isEmpty();
+//        Asserts.assertThat(actual).isNotNull().isNotEmpty();
+        AbstractStringAssert<?, String> stringAbstractStringAssert = Asserts.assertThat(actual);
 
     }
 
@@ -30,7 +31,8 @@ public class AssertsBasicTest {
         String actual = "hello";
         AbstractStringAssert<?, String> stringAbstractStringAssert = Asserts.assertThat(actual);
 
-        Assertions.assertThat(stringAbstractStringAssert).isInstanceOf(AbstractInstanceAssert.class);
+        Assertions.assertThat(stringAbstractStringAssert)
+            .isInstanceOf(AbstractInstanceAssert.class);
     }
 
 }

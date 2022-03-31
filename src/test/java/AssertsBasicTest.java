452f2ede42367f667a4b089512baf97e00800950
ones1kk <kwsic@naver.com>
2022-03-31 23:30:28 +0900

    ✅ Add : Default Struncture Test

--- src/test/java/AssertsBasicTest.java
+++ src/test/java/AssertsBasicTest.java
@@ -1,6 +1,8 @@
-import asssert.Asserts;
-import asssert.core.AbstractInstanceAssert;
+import asssert.Assertion;
 import asssert.core.AbstractStringAssert;
+import asssert.core.CharacterAssert;
+import asssert.core.StringAssert;
+import asssert.core.handler.CharSequenceAssert;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,32 +10,17 @@ import org.junit.jupiter.api.Test;
 public class AssertsBasicTest {
 
     @Test
-    @DisplayName("Assertions")
-    void test1() {
-        String str = "hello";
-        Assertions.assertThat(str).as("actual was {}", str).isNotNull().isNotEmpty().isNotBlank()
-            .isEqualTo("hello");
-    }
-
-    @Test
     @DisplayName("Basic Structure")
-    void test2() {
+    void test1() {
         String actual = "hello";
+        Assertions.assertThat(Assertion.assertThat(actual).isNotNull().isNotEmpty())
+            .isExactlyInstanceOf(StringAssert.class);
 
-//        Asserts.assertThat(actual).isNotNull().isNotEmpty();
-        AbstractStringAssert<?, String> stringAbstractStringAssert = Asserts.assertThat(actual);
-
+        char actual1 = 'h';
+        Assertions.assertThat(Assertion.assertThat(actual1).isUpperCase()).isExactlyInstanceOf(
+            CharacterAssert.class);
     }
 
-    @Test
-    @DisplayName("Is this assertion instance of AbstractInstanceAssert?")
-    void test3() {
-        String actual = "hello";
-        AbstractStringAssert<?, String> stringAbstractStringAssert = Asserts.assertThat(actual);
-
-        Assertions.assertThat(stringAbstractStringAssert)
-            .isInstanceOf(AbstractInstanceAssert.class);
-    }
 
 }
 

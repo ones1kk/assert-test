738b8c5bee9a6deb41f1ba794f83eecc21992ac9
ones1kk <kwsic@naver.com>
2022-04-02 00:27:38 +0900

    🔥 Remove : File

--- src/main/java/asssert/core/handler/CharSequenceAssert.java
+++
@@ -1,9 +0,0 @@
-package asssert.core.handler;
-
-public class CharSequenceAssert<SELF, ACTUAL> extends Assert<SELF, ACTUAL> {
-
-    protected CharSequenceAssert(Class<?> self, ACTUAL actual) {
-        super(self, actual);
-    }
-
-}

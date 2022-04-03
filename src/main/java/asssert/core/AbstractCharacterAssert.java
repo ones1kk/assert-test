6702118ecd7bac5c5e679d97e978724e54d8bc47
ones1kk <kwsic@naver.com>
2022-04-03 23:05:01 +0900

    ✨ add : AbstractCharacterAssert

--- src/main/java/asssert/core/AbstractCharacterAssert.java
+++ src/main/java/asssert/core/AbstractCharacterAssert.java
@@ -1,29 +1,40 @@
 package asssert.core;
 
-public class AbstractCharacterAssert<SELF extends AbstractCharacterAssert<SELF, ACTUAL>, ACTUAL> extends
-    AbstractObjectAssert<SELF, ACTUAL> {
+import asssert.core.handler.AnonymousCharacterAssert;
+import asssert.core.handler.AssertFactory;
 
-    protected AbstractCharacterAssert(Class<?> self, ACTUAL actual) {
+public class AbstractCharacterAssert<SELF extends AbstractCharacterAssert<SELF, Character>, ACTUAL> extends
+    AbstractObjectAssert<SELF, Character> {
+
+    protected AnonymousCharacterAssert characterAssert ;
+
+    protected AbstractCharacterAssert(Class<?> self, Character actual) {
         super(self, actual);
+        this.characterAssert = (AnonymousCharacterAssert) AssertFactory.createAssert(actual);
     }
 
     public SELF isUpperCase() {
+        this.characterAssert.isUpperCase(actual);
         return self;
     }
 
     public SELF isLowerCase() {
+        this.characterAssert.isLowerCase(actual);
         return self;
     }
 
     public SELF isLetter() {
+        this.characterAssert.isLowerCase(actual);
         return self;
     }
 
     public SELF isDigit() {
+        this.characterAssert.isDigit(actual);
         return self;
     }
 
     public SELF isWhitespace() {
+        this.characterAssert.isWhitespace(actual);
         return self;
     }
 }

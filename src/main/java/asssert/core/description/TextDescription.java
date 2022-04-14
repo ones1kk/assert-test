e7a1dcb918a157bd368e0ec7b819fc4d21ce769c
ones1kk <kwsic@naver.com>
2022-04-14 23:08:59 +0900

    ✨ Add : Text Format

--- src/main/java/asssert/core/description/TextDescription.java
+++ src/main/java/asssert/core/description/TextDescription.java
@@ -1,5 +1,22 @@
 package asssert.core.description;
 
+import javax.annotation.Nullable;
+
 public class TextDescription extends Description {
 
+    public String setDefaultText(Object actual, @Nullable Object expected, String description) {
+        if (expected != null) {
+            return String.format(
+                "\n" + WHITE + "Expected    : %s \n" + WHITE + "Actual      : %s\n" + WHITE
+                    + "Description : %s",
+                RED + expected, RED + actual, RED + description);
+        } else {
+            return String.format(
+                "\n" + WHITE + "Actual      : %s\n" + WHITE
+                    + "Description : %s",
+                RED + actual, RED + description);
+        }
+    }
 }
+
+

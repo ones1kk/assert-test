60b97af4b01808a38367a81d4cc861144ef61b16
ones1kk <kwsic@naver.com>
2022-04-14 21:04:49 +0900

    ✅ Add : As description

--- src/test/java/asssert/core/description/DescriptionTest.java
+++ src/test/java/asssert/core/description/DescriptionTest.java
@@ -28,4 +28,22 @@ class DescriptionTest {
         assertThat(message).isEqualTo("hello it's kind of test");
     }
 
+    @Test
+    @DisplayName("Can replace description of exception?")
+    void test2() {
+        String actual = "hello";
+
+        String message = Assertions.assertThrows(RuntimeException.class,
+            () -> Assertion.assertThat(actual).isNull()).getMessage();
+
+        assertThat(message).isEqualTo("hello is not null");
+
+        String asDescription = Assertions.assertThrows(RuntimeException.class,
+            () -> Assertion.assertThat(actual).as("this is as {}", "description").isNull()
+        ).getMessage();
+
+        assertThat(asDescription).isEqualTo("this is as description");
+
+    }
+
 }

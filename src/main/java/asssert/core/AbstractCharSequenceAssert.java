280c362f1fedd30adfceb8e75494bef4eeedd72f
ones1kk <kwsic@naver.com>
2022-04-19 23:29:58 +0900

    ♻️ Refactor : CharSequenceAssert

--- src/main/java/asssert/core/AbstractCharSequenceAssert.java
+++ src/main/java/asssert/core/AbstractCharSequenceAssert.java
@@ -1,10 +1,25 @@
 package asssert.core;
 
-public abstract class AbstractCharSequenceAssert<SELF extends AbstractCharSequenceAssert<SELF, ACTUAL>, ACTUAL> extends
+import asssert.core.handler.AnonymousCharSequenceAssert;
+
+public abstract class AbstractCharSequenceAssert<SELF extends AbstractCharSequenceAssert<SELF, ACTUAL>, ACTUAL extends CharSequence> extends
     AbstractObjectAssert<SELF, ACTUAL> {
+
+    protected AnonymousCharSequenceAssert charSequenceAssert;
 
     protected AbstractCharSequenceAssert(Class<?> self, ACTUAL actual) {
         super(self, actual);
+        this.charSequenceAssert = assertFactory.createAssert(actual);
+    }
+
+    public SELF isEmpty() {
+        this.charSequenceAssert.isEmpty((String) actual);
+        return self;
+    }
+
+    public SELF isNotEmpty() {
+        this.charSequenceAssert.isNotEmpty((String) actual);
+        return self;
     }
 
 
--- src/main/java/asssert/core/handler/AnonymousCharSequenceAssert.java
+++ src/main/java/asssert/core/handler/AnonymousCharSequenceAssert.java
@@ -1,6 +1,24 @@
 package asssert.core.handler;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class AnonymousCharSequenceAssert extends AbstractAssert {
 
+    public void isEmpty(String actual) {
+        boolean result = StringUtils.isEmpty(actual);
+        this.defaultDescription = actual + " is not empty";
+        if (!result) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, null, description));
+        }
+    }
 
+    public void isNotEmpty(String actual) {
+        boolean result = StringUtils.isNotEmpty(actual);
+        this.defaultDescription = actual + " is empty";
+        if (!result) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, null, description));
+        }
+    }
 }

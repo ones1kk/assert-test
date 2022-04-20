b2c3bbea800c5005080fcd09e2ea04d8a481797b
ones1kk <kwsic@naver.com>
2022-04-20 23:18:33 +0900

    ✨ Make : Change of code location

--- src/main/java/asssert/core/handler/AnonymousObjectAssert.java
+++ src/main/java/asssert/core/handler/AnonymousObjectAssert.java
@@ -1,6 +1,58 @@
 package asssert.core.handler;
 
+import java.util.Objects;
+
 public class AnonymousObjectAssert extends AbstractAssert {
+
+    @Override
+    public void isNotNull(Object actual) {
+        boolean result = actual != null;
+        this.defaultDescription = actual + " is null";
+        if (!result) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, null, description));
+        }
+    }
+
+    @Override
+    public void isSameAs(Object actual, Object expected) {
+        boolean result = actual == expected;
+        this.defaultDescription = (actual + " is not same as " + expected);
+        if (!result) {
+            String description = setDescription();
+            throw getException(setDefaultText(expected, actual, description));
+        }
+    }
+
+    @Override
+    public void isEqualTo(Object actual, Object expected) {
+        boolean result = (Objects.deepEquals(actual, expected));
+        this.defaultDescription = (actual + " is not equal to " + expected);
+        if (!result) {
+            String description = setDescription();
+            throw getException(setDefaultText(expected, actual, description));
+        }
+    }
+
+    @Override
+    public void isNotEqualTo(Object actual, Object expected) {
+        boolean result = !(Objects.deepEquals(actual, expected));
+        this.defaultDescription = (actual + " is equal to " + expected);
+        if (!result) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, expected, description));
+        }
+    }
+
+    @Override
+    public void isAssignableFrom(Object actual, Class<?> expected) {
+        boolean result = actual.getClass().isAssignableFrom(expected);
+        this.defaultDescription = (actual + " is not assignable from " + expected);
+        if (!result) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual.getClass(), expected, description));
+        }
+    }
 
 
 }

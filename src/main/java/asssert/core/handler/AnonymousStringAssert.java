9c1efa32770b7dc0ad975cd5a98c7627f1a682ff
ones1kk <kwsic@naver.com>
2022-04-20 23:18:57 +0900

    ✨ Add : AnonymouseStringAssert methods

--- src/main/java/asssert/core/handler/AnonymousStringAssert.java
+++ src/main/java/asssert/core/handler/AnonymousStringAssert.java
@@ -1,33 +1,57 @@
 package asssert.core.handler;
 
-import asssert.core.feature.Comparable;
+import asssert.core.feature.ComparableAssert;
 
 public class AnonymousStringAssert extends AnonymousCharSequenceAssert implements
-    Comparable<String> {
+    ComparableAssert<String> {
 
     @Override
-    public void isLessThan(String actual, Object expected) {
-
+    public void isLessThan(String actual, String expected) {
+        int result = actual.compareTo(expected);
+        this.defaultDescription = actual + " is greater than " + expected;
+        if (result != -1) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, expected, description));
+        }
     }
 
     @Override
-    public void isLessThanOrEqualTo(String actual, Object expected) {
-
+    public void isLessThanOrEqualTo(String actual, String expected) {
+        int result = actual.compareTo(expected);
+        this.defaultDescription = actual + " is greater than or Equal to " + expected;
+        if (result == 1) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, expected, description));
+        }
     }
 
     @Override
-    public void isGreaterThan(String actual, Object expected) {
-
+    public void isGreaterThan(String actual, String expected) {
+        int result = actual.compareTo(expected);
+        this.defaultDescription = actual + " is less than " + expected;
+        if (result != 1) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, expected, description));
+        }
     }
 
     @Override
-    public void isGreaterThanOrEqualTo(String actual, Object expected) {
-
+    public void isGreaterThanOrEqualTo(String actual, String expected) {
+        int result = actual.compareTo(expected);
+        this.defaultDescription = actual + " is less than or Equal to " + expected;
+        if (result == -1) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, expected, description));
+        }
     }
 
     @Override
-    public void isBetween(String actual, Object start, Object end) {
-
+    public void isBetween(String actual, String start, String end) {
+        if ((actual.compareTo(start) == -1) && ((
+            actual.compareTo(end) == 1))) {
+            this.defaultDescription = actual + " is not between " + start + "  and " + end;
+            String description = setDescription();
+            throw getException(setDefaultText(actual, start + " ~ " + end, description));
+        }
     }
-
 }

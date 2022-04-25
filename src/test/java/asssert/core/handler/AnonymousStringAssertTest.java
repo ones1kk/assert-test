3948c6f16946fd5bee684099a217763e24cf10ab
ones1kk <ons1k95@gmail.com>
2022-04-26 00:03:47 +0900

    ✅ Add : AnnoymousStringAssertTest(success & fail scenario)

--- src/test/java/asssert/core/handler/AnonymousStringAssertTest.java
+++ src/test/java/asssert/core/handler/AnonymousStringAssertTest.java
@@ -1,13 +1,15 @@
 package asssert.core.handler;
 
 import asssert.Assertion;
+import asssert.core.exception.AssertException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AnonymousStringAssertTest {
 
     @Test
-    @DisplayName("AnonymousStringAssert operators")
+    @DisplayName("AnonymousStringAssert operators(success)")
     void test1() {
         String actual1 = "A";
         String actual2 = "B";
@@ -17,14 +19,61 @@ class AnonymousStringAssertTest {
         String expected2 = "B";
         String expected3 = "C";
 
-        Assertion.assertThat(actual1).isLessThan(expected2);
-        Assertion.assertThat(actual2).isGreaterThan(expected1);
+        AnonymousStringAssert stringAssert = new AnonymousStringAssert();
 
-        Assertion.assertThat(actual1).isLessThanOrEqualTo(expected1);
-        Assertion.assertThat(actual1).isLessThanOrEqualTo(expected2);
+        // actual < expected
+        stringAssert.isLessThan(actual1, expected2);
+        // actual > expected
+        stringAssert.isGreaterThan(actual2, expected1);
 
-        Assertion.assertThat(actual3).isGreaterThanOrEqualTo(expected3);
-        Assertion.assertThat(actual3).isGreaterThanOrEqualTo(expected2);
+        // actual == expected
+        stringAssert.isLessThanOrEqualTo(actual1, expected1);
+        // actual < expected
+        stringAssert.isLessThanOrEqualTo(actual1, expected2);
+
+        // actual == expected
+        stringAssert.isGreaterThanOrEqualTo(actual3, expected3);
+        // actual >= expected
+        stringAssert.isGreaterThanOrEqualTo(actual3, expected2);
+
+        // start < actual < end
+        stringAssert.isBetween(actual2, expected1, expected3);
+
+    }
+
+    @Test
+    @DisplayName("AnonymousStringAssert operators(fail)")
+    void test2() {
+        String actual1 = "A";
+        String actual2 = "B";
+        String actual3 = "C";
+
+        String expected1 = "A";
+        String expected2 = "B";
+        String expected3 = "C";
+
+        AnonymousStringAssert stringAssert = new AnonymousStringAssert();
+
+        // actual > expected
+        Assertions.assertThrows(AssertException.class, () -> stringAssert.isLessThan(actual2, expected1));
+        // actual == expected
+        Assertions.assertThrows(AssertException.class, () -> stringAssert.isLessThan(actual2, expected2));
+
+        // actual < expected
+        Assertions.assertThrows(AssertException.class, () -> stringAssert.isGreaterThan(actual1, expected2));
+        // actual == expected
+        Assertions.assertThrows(AssertException.class, () -> stringAssert.isGreaterThan(actual1, expected1));
+
+        // actual > expected
+        Assertions.assertThrows(AssertException.class, () -> stringAssert.isLessThanOrEqualTo(actual2, expected1));
+
+        // actual < expected
+        Assertions.assertThrows(AssertException.class, () -> stringAssert.isGreaterThanOrEqualTo(actual2, expected3));
+
+        // actual < start
+        Assertions.assertThrows(AssertException.class, () -> stringAssert.isBetween(actual1, expected2, expected3));
+        // actual > end
+        Assertions.assertThrows(AssertException.class, () -> stringAssert.isBetween(actual3, expected1, expected2));
 
     }
 

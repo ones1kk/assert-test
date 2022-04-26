8c8c878c15da828872274a8e58354c5a2563207a
ones1kk <ones1k95@gmail.com>
2022-04-27 00:17:29 +0900

    ✨ Add : IntegerAssert Methods

--- src/main/java/asssert/core/AbstractIntegerAssert.java
+++ src/main/java/asssert/core/AbstractIntegerAssert.java
@@ -11,4 +11,49 @@ public class AbstractIntegerAssert<SELF extends AbstractIntegerAssert<SELF, Inte
         super(self, actual);
         this.integerAssert = assertFactory.createAssert(actual);
     }
+
+    public SELF isPositive() {
+        this.integerAssert.isPositive(actual);
+        return self;
+    }
+
+    public SELF isNegative() {
+        this.integerAssert.isNegative(actual);
+        return self;
+    }
+
+    public SELF isEven() {
+        this.integerAssert.isEven(actual);
+        return self;
+    }
+
+    public SELF isOdd() {
+        this.integerAssert.isOdd(actual);
+        return self;
+    }
+
+    public SELF isLessThan(Integer expected) {
+        this.integerAssert.isLessThan(actual, expected);
+        return self;
+    }
+
+    public SELF isLessThanOrEqualTo(Integer expected) {
+        this.integerAssert.isLessThanOrEqualTo(actual, expected);
+        return self;
+    }
+
+    public SELF isGreaterThan(Integer expected) {
+        this.integerAssert.isGreaterThan(actual, expected);
+        return self;
+    }
+
+    public SELF isGreaterThanOrEqualTo(Integer expected) {
+        this.integerAssert.isGreaterThanOrEqualTo(actual, expected);
+        return self;
+    }
+
+    public SELF isBetween(Integer start, Integer end) {
+        this.integerAssert.isBetween(actual, start, end);
+        return self;
+    }
 }
--- src/main/java/asssert/core/handler/AnonymousIntegerAssert.java
+++ src/main/java/asssert/core/handler/AnonymousIntegerAssert.java
@@ -1,5 +1,93 @@
 package asssert.core.handler;
 
-public class AnonymousIntegerAssert extends AnonymousObjectAssert {
+import asssert.core.feature.ComparableAssert;
 
+public class AnonymousIntegerAssert extends AnonymousObjectAssert implements
+    ComparableAssert<Integer> {
+
+    public void isPositive(Integer actual) {
+        boolean result = actual > 0;
+        this.defaultDescription = actual + " is not positive";
+        if (!result) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, "Positive", description));
+        }
+    }
+
+    public void isNegative(Integer actual) {
+        boolean result = actual < 0;
+        this.defaultDescription = actual + " is not negative";
+        if (!result) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, "Negative", description));
+        }
+    }
+
+    public void isEven(Integer actual) {
+        boolean result = (actual & 1) == 0;
+        this.defaultDescription = actual + " is not even";
+        if (!result) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, "Odd", description));
+        }
+    }
+
+    public void isOdd(Integer actual) {
+        boolean result = (actual & 1) != 0;
+        this.defaultDescription = actual + " is not odd";
+        if (!result) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, "Even", description));
+        }
+    }
+
+    @Override
+    public void isLessThan(Integer actual, Integer expected) {
+        int result = actual.compareTo(expected);
+        this.defaultDescription = actual + " is not less than " + expected;
+        if (result != -1) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, expected, description));
+        }
+    }
+
+    @Override
+    public void isLessThanOrEqualTo(Integer actual, Integer expected) {
+        int result = actual.compareTo(expected);
+        this.defaultDescription = actual + " is not less than or Equal to " + expected;
+        if (result == 1) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, expected, description));
+        }
+    }
+
+    @Override
+    public void isGreaterThan(Integer actual, Integer expected) {
+        int result = actual.compareTo(expected);
+        this.defaultDescription = actual + " is not greater than " + expected;
+        if (result != 1) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, expected, description));
+        }
+    }
+
+    @Override
+    public void isGreaterThanOrEqualTo(Integer actual, Integer expected) {
+        int result = actual.compareTo(expected);
+        this.defaultDescription = actual + " is not greater than or Equal to " + expected;
+        if (result == -1) {
+            String description = setDescription();
+            throw getException(setDefaultText(actual, expected, description));
+        }
+    }
+
+    @Override
+    public void isBetween(Integer actual, Integer start, Integer end) {
+        if ((actual.compareTo(start) == -1) || ((
+            actual.compareTo(end) == 1))) {
+            this.defaultDescription = actual + " is not between " + start + "  and " + end;
+            String description = setDescription();
+            throw getException(setDefaultText(actual, start + " ~ " + end, description));
+        }
+    }
 }

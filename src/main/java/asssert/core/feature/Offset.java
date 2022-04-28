1fba8c1e72b823a64aa6af21605f7f342b9cc959
ones1kk <ones1k95@gmail.com>
2022-04-29 00:46:37 +0900

    ✨ Add : Offset

---
+++ src/main/java/asssert/core/feature/Offset.java
@@ -0,0 +1,16 @@
+package asssert.core.feature;
+
+
+public final class Offset<T extends Number> {
+
+    public final T value;
+
+    private Offset(T value) {
+        this.value = value;
+    }
+
+    public static <T extends Number> Offset<T> offset(T value) {
+        if (value == null) throw new NullPointerException("offset value should be not null");
+        return new Offset<>(value);
+    }
+}

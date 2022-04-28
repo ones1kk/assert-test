5cf68fa3e3d42d84b397801002c75ef8f5f48916
ones1kk <ones1k95@gmail.com>
2022-04-29 00:47:02 +0900

    ✅ Add : OffsetTest

---
+++ src/test/java/asssert/core/feature/OffsetTest.java
@@ -0,0 +1,27 @@
+package asssert.core.feature;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OffsetTest {
+    
+    @Test
+    @DisplayName("Basic Test")
+    void test1() {
+        Offset<Integer> integerOffset = Offset.offset(1);
+        Offset<Double> doubleOffset = Offset.offset(1.1);
+        Offset<Float> floatOffset = Offset.offset(1.1f);
+        Offset<Long> longOffset = Offset.offset(1L);
+
+
+        Assertions.assertThat(integerOffset.value).isNotNull().isEqualTo(1);
+        Assertions.assertThat(doubleOffset.value).isNotNull().isEqualTo(1.1);
+        Assertions.assertThat(floatOffset.value).isNotNull().isEqualTo(1.1f);
+        Assertions.assertThat(longOffset.value).isNotNull().isEqualTo(1L);
+
+    }
+
+}
